df9555ac94e9504b2fd7e51ba8ca64d487dcee29
Goldee Arora <200104027@hbtu.ac.in>
2022-07-23 11:07:43 +0530

    commiting all the files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "uploadimages34c606527": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "imagebuckets3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import { Storage } from "aws-amplify";
+// import DeleteIcon from "@mui/icons-material/Delete";
 function App() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  // console.log(images);
+  async function fetchImages() {
+    let imageKeys = await Storage.list("");
+    imageKeys = await Promise.all(
+      imageKeys.map(async (k) => {
+        const key = await Storage.get(k.key);
+        return key;
+      })
+    );
+    // console.log("imageKeys: ", imageKeys);
+    setImages(imageKeys);
+  }
+  async function onChange(e) {
+    const file = e.target.files[0];
+    // console.log(file.name);
+    const result = await Storage.put(file.name, file, {
+      contentType: "image/png",
+    });
+    console.log({ result });
+    fetchImages();
+  }
+  async function deleteImage(e) {
+    await Storage.remove(
+      e.target.src.substring(
+        e.target.src.indexOf("/public") + 8,
+        e.target.src.indexOf("?")
+      )
+    );
+    fetchImages();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {images.map((image) => {
+          return (
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <img
+                src={image}
+                key={image}
+                style={{ width: 50, height: 50, marginBottom: 10 }}
+                onClick={deleteImage}
+              />
+              {/* <button onClick={deleteImage}>del</button> */}
+            </div>
+          );
+        })}
+      </div>
+      <input type="file" onChange={onChange} />
     </div>
   );
 }
